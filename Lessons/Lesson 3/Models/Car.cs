9b16d9b1d3a0bd7745c9fe07f32ad4e31ea79f59
diogodelmar <a31541@alunos.ipca.pt>
2025-10-03 08:36:13 +0100

    aula3

---
+++ Lessons/Lesson 3/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace Lesson_2;
+
+public class Car
+{
+    // Definir variáveis
+    public string LicensePlate { get; }
+    public string Color { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string NrOfDoors { get; set; }
+
+    public void Start()
+    {
+        Console.WriteLine("Car Start");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Car Stop");
+    }
+    public void Display()
+    {
+        Console.WriteLine($"License Plate");
+    }
+
+
+
+
+
+
+
+}
